08a142914d5251dbe2ca09b0993fa6943275ec9e
Zolihhh <szabozoltan1@ugmszi.hu>
2026-01-30 08:49:43 +0100

    LuresTest javitas

--- server/tests/Unit/LuresTest.php
+++ server/tests/Unit/LuresTest.php
@@ -1,46 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-namespace Tests\Unit;
-
-use Illuminate\Support\Facades\Schema;
-use Tests\TestCase;
-use PHPUnit\Framework\Attributes\DataProvider;
-
-class LuresTest extends TestCase
-{
-    protected $table = 'lures';
-
-    public static function expectedSchemaDataProvider()
-    {
-        return [
-            ['id', ['bigint', 'integer']],
-            ['lure', 'varchar'],
-        ];
-    }
-
-    public function test_lures_table_exists(): void
-    {
-        $this->assertTrue(Schema::hasTable($this->table));
-    }
-
-    #[DataProvider('expectedSchemaDataProvider')]
-    public function test_lures_columns_exist(string $column): void
-    {
-        $this->assertTrue(Schema::hasColumn($this->table, $column));
-    }
-
-    #[DataProvider('expectedSchemaDataProvider')]
-    public function test_lures_column_types(string $column, $expectedType): void
-    {
-        $actualType = Schema::getColumnType($this->table, $column);
-
-        if (is_array($expectedType)) {
-            $this->assertTrue(in_array($actualType, $expectedType));
-        } else {
-            $this->assertEquals($expectedType, $actualType);
-        }
-=======
 namespace Tests\Feature;
 
 use App\Models\Lure;
@@ -98,6 +57,5 @@ class LuresTest extends TestCase
         $this->assertDatabaseMissing('lures', [
             'id' => $lure->id,
         ]);
->>>>>>> 2b61c8e03fa8a3fccd669c6b9412da37df5c2efb
     }
 }
