bea83a324c97abcf419d56ea43e72d828e03d61a
Sebokbence586 <sebokbence2006@gmail.com>
2026-03-04 08:32:43 +0100

    kész

--- client/src/components/Layout/Header.vue
+++ client/src/components/Layout/Header.vue
@@ -1,58 +1,7 @@
-<!-- <template>
+<template></template>
 
-    <header class="hero-header">
-
-        <div class="hero-overlay">
-
-            <h1>🎣 Horgász Napló</h1>
-
-            <p>Rögzítsd a fogásaid és kövesd a horgászatodat</p>
-
-        </div>
-
-    </header>
-
-</template> -->
-
-
-<style scoped>
-.hero-header {
-
-    height: 220px;
-
-    background-image: url("https://images.unsplash.com/photo-1504274066651-8d31a536b11a"
-        );
-
-    background-size: cover;
-    background-position: center;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-}
-
-
-
-.hero-overlay {
-
-    background: rgba(0, 0, 0, 0.55);
-
-    padding: 30px 50px;
-
-    border-radius: 12px;
-
-    color: white;
-
-    text-align: center;
-
-}
-
-
-
-.hero-overlay h1 {
-
-    font-size: 36px;
-
-}
-</style>
+<script>
+export default {
+  name: "HeaderBar",
+};
+</script>
